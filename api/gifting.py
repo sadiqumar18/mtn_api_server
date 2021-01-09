fa73d349f4fb47eea265f31daf5b390ced65273e
sadiq <sadiqumar18@gmail.com>
2021-01-09 12:57:59 +0100

    fix

--- api/gifting.py
+++ api/gifting.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.headless = False
 options.add_argument('--no-sandbox')
-options.add_argument("--remote-debugging-port=9222")
+#options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
