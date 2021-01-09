8f4f988efc487d62877a4a4dac1a32a061a19236
sadiq <sadiqumar18@gmail.com>
2021-01-09 09:43:08 +0100

    fix

--- api/gifting.py
+++ api/gifting.py
@@ -6,9 +6,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 
+
 options = Options()
 options.headless = False
 options.add_argument('--no-sandbox')
+options.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
