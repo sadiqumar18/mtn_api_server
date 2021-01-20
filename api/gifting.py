8bc7434aef5c2b7571ecf9f55cfb63747fcc23a0
sadiqumar18 <sadiqumar18@gmail.com>
2021-01-20 15:12:20 +0000

    test

--- api/gifting.py
+++ api/gifting.py
@@ -15,7 +15,7 @@ options.headless = False
 options.add_argument('--no-sandbox')
 # options.add_argument("--remote-debugging-port=9222")
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--blink-settings=imagesEnabled=false')
+#options.add_argument('--blink-settings=imagesEnabled=false')
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
